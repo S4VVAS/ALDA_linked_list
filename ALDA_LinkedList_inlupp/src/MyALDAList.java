c425f5417139221c52888259ac5a0f14054f0dc2
Savvas <savvasswe@gmail.com>
2020-01-24 15:18:57 +0100

    Added comments/ javadoc descriptions to code

--- ALDA_LinkedList_inlupp/src/MyALDAList.java
+++ ALDA_LinkedList_inlupp/src/MyALDAList.java
@@ -1,11 +1,13 @@
+//Savvas Giortsis (Sagi2536) savvasswe@gmail.com
+
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class MyALDAList<T> implements ALDAList<T> {
 
-	private final Node<T> first = new Node<T>(null);
-	private final Node<T> last = new Node<T>(null);
+	private final Node<T> first = new Node<T>(null); // Starting sentinel node.
+	private final Node<T> last = new Node<T>(null); // Ending sentinel node.
 	private Node<T> lastElement = first;
 	private int currentOperations = 0;
 	private int size = 0;
@@ -14,7 +16,7 @@ public class MyALDAList<T> implements ALDAList<T> {
 		first.nextNode = last;
 	}
 
-	/** Adds a specified element of a type, into the linked list of that type*/
+	/** Adds an element into the end of list. */
 	@Override
 	public void add(T element) {
 		Node<T> newNode = new Node<T>(element);
@@ -25,6 +27,11 @@ public class MyALDAList<T> implements ALDAList<T> {
 		currentOperations++;
 	}
 
+	/**
+	 * Adds an element into the specified index of the list, pushing elements above
+	 * that index up. Can cast IndexOutOfBoundsException() if the index does not
+	 * exist in list (bellow 0 or above max index)
+	 */
 	@Override
 	public void add(int index, T element) {
 		Node<T> node = first;
@@ -33,7 +40,7 @@ public class MyALDAList<T> implements ALDAList<T> {
 				Node<T> newNode = new Node<T>(element);
 				newNode.nextNode = node.nextNode;
 				node.nextNode = newNode;
-				if(i == size-1)
+				if (i == size - 1)
 					lastElement = newNode;
 				currentOperations++;
 				size++;
@@ -43,46 +50,64 @@ public class MyALDAList<T> implements ALDAList<T> {
 		throw new IndexOutOfBoundsException();
 	}
 
+	/**
+	 * Finds and removes the specified element in the list, on successful operation,
+	 * true is returned. Else if that element does not exist, false is returned
+	 */
 	@Override
 	public boolean remove(T element) {
 		Node<T> node = first;
-		for (int i = -1; i < size-1; i++, node = node.nextNode) {
+		for (int i = -1; i < size - 1; i++, node = node.nextNode) {
 			if (node.nextNode.data.equals(element)) {
-				delete(node);
+				deleteFromPrevious(node);
 				return true;
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Removes the element on the specified index and returns that removed element
+	 * to the caller. Can cast IndexOutOfBoundsException() if the index does not
+	 * exist in list (bellow 0 or above max index)
+	 */
 	@Override
 	public T remove(int index) {
 		Node<T> node = first;
-		for (int i = -1; i < size-1; i++, node = node.nextNode) {
-			if (i == index-1) {
+		for (int i = -1; i < size - 1; i++, node = node.nextNode) {
+			if (i == index - 1) {
 				Node<T> removedNode = node.nextNode;
-				delete(node);
+				deleteFromPrevious(node);
 				return removedNode.data;
 			}
 		}
 		throw new IndexOutOfBoundsException();
 	}
 
-	private void delete(Node<T> previousNode) {
-		if(previousNode.nextNode == lastElement) 
+	// Deletes node from previous, avoids reiteration of the list in order to find
+	// the previous element.
+	private void deleteFromPrevious(Node<T> previousNode) {
+		if (previousNode.nextNode == lastElement)
 			lastElement = previousNode;
-		
 		Node<T> nextNode = previousNode.nextNode.nextNode;
 		previousNode.nextNode = nextNode;
 		size--;
 		currentOperations++;
 	}
 
+	/**
+	 * Returns: true or false based on if the specified element exists in the list
+	 * or not.
+	 */
 	@Override
 	public boolean contains(T element) {
 		return indexOf(element) != -1;
 	}
 
+	/**
+	 * Returns: the index in the list where the specified element is. Returns: -1 if
+	 * there is no instances of that element in list.
+	 */
 	@Override
 	public int indexOf(T element) {
 		Node<T> node = first;
@@ -94,11 +119,17 @@ public class MyALDAList<T> implements ALDAList<T> {
 		return -1;
 	}
 
+	/** Returns: a new iterator for this list. */
 	@Override
 	public Iterator<T> iterator() {
 		return new ListIterator<T>(this);
 	}
 
+	/**
+	 * Returns: the value of a certain index in this list. Can cast
+	 * IndexOutOfBoundsException() if the index does not exist in list (bellow 0 or
+	 * above max index)
+	 */
 	@Override
 	public T get(int index) {
 		Node<T> node = first.nextNode;
@@ -109,6 +140,7 @@ public class MyALDAList<T> implements ALDAList<T> {
 		throw new IndexOutOfBoundsException();
 	}
 
+	/** Clears all elements in list (Deletes everything). */
 	@Override
 	public void clear() {
 		first.nextNode = last;
@@ -117,22 +149,23 @@ public class MyALDAList<T> implements ALDAList<T> {
 		currentOperations++;
 	}
 
+	/** Returns: the number of elements in the list */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/** Returns: a string representation of the list. */
 	@Override
 	public String toString() {
 		String str = "[";
 		Iterator<T> it = this.iterator();
 		boolean firstElm = true;
-		while(it.hasNext())
+		while (it.hasNext())
 			if (firstElm) {
 				str = str + it.next();
 				firstElm = false;
-			}
-			else
+			} else
 				str = str + ", " + it.next();
 		str = str + "]";
 		return str;
@@ -141,8 +174,8 @@ public class MyALDAList<T> implements ALDAList<T> {
 	// ____INNER CLASSES BELLOW_______
 
 	private static class Node<T> {
-		Node<T> nextNode = null;
-		T data;
+		protected Node<T> nextNode = null;
+		protected T data;
 
 		public Node(T data) {
 			this.data = data;
